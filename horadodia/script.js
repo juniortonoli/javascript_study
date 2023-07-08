e08176a94d90e050706e456fa28a9963761358fd
Wagner Tonoli Junior <tailsapnea@gmail.com>
2023-07-08 15:27:55 -0300

    Add files via upload

---
+++ horadodia/script.js
@@ -0,0 +1,14 @@
+function carregar(){
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12){img.src = "manha.png"
+document.body.style.background = '#a8a854'}
+    else if (hora >= 12 && hora < 18){img.src = "tarde.png"
+    document.body.style.background = '#fbff00'}
+    else {img.src = "noite.png"
+    document.body.style.background = '#2e2e28'}
+
+}
