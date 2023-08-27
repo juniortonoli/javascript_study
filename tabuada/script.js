89285c518dc04f412594f6a05bca03dff3fde418
Wagner Tonoli Junior <tailsapnea@gmail.com>
2023-08-27 00:06:02 -0300

    Create script.js

---
+++ tabuada/script.js
@@ -0,0 +1,11 @@
+function contar(){
+var num = Number(document.getElementById('numero').value)
+var tab = document.getElementById('seltab')
+tab.innerHTML = ''
+for (var c = 1; c <= 10; c++ ){
+    var item = document.createElement('option')
+    item.text = `${num} x ${c} = ${num*c}`
+    tab.appendChild(item)
+}
+
+}
